4884e2fd295e73f1af7ca1f2fd883fc5260b7b04
Zij Zahidul Islam Jiku <safzahidul.cse@gmail.com>
2023-03-20 19:54:17 +0600

    filter action setup

---
+++ redux/filters/action.js
@@ -0,0 +1,18 @@
+import { STATUSCHANGED, COLORCHANGED } from './actionTypes';
+
+export const statusChanged = (status) => {
+    return {
+        type: STATUSCHANGED,
+        payload: status
+    } 
+}
+
+export const colorChanged = (color, changeType) => {
+    return {
+        type: COLORCHANGED,
+          payload: {
+            color,
+            changeType
+        }
+    } 
+}
