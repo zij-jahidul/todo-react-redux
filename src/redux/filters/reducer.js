bbafe909f1368fb410d0de603b39b7253d0c703f
Zij Zahidul Islam Jiku <safzahidul.cse@gmail.com>
2023-03-20 20:54:13 +0600

    Filter Reducer build done

---
+++ src/redux/filters/reducer.js
@@ -0,0 +1,42 @@
+import { initialState } from "./initialState";
+import { STATUSCHANGED, COLORCHANGED } from './actionTypes';
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case STATUSCHANGED:
+            return {
+                ...state,
+                status: action.payload
+            }
+        
+        case COLORCHANGED:
+            const { color, changeType } = action.payload;
+
+            switch (changeType) {
+                case 'added':
+                    return {
+                        ...state,
+                        colors: [
+                            ...state.colors,
+                            color
+                        ]
+                    }
+
+
+                case 'removed':
+                    return {
+                        ...state,
+                        colors: state.colors.filter(existingColor => existingColor !== color)
+                    }
+
+
+                default:
+                    state;
+            }
+        
+        default:
+            return state;
+    }
+}
+
+export default reducer;
